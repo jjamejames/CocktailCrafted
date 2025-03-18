58064669b2671cb6f9357ab54caae47bd7b17f2f
James Anurak <jameanurak.wa@gmail.com>
2025-03-18 21:58:10 +0700

    design header and footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,18 @@
-function Footer(){
-    return(
-        <div>
-            Footer
-        </div>
-    )
-}
+function Footer() {
+  return (
+    <div className="footer w-100% h-[50px] bg-[#4D0000] flex items-center">
+      <span className="flex-1 flex justify-start ml-[30px]">
+        Developed By James Anurak
+      </span>
 
-export default Footer
+      <a
+        href="mailto:jameanurak.wa@gmail.com"
+        className="flex-1 flex justify-center"
+      >
+        Contact Me
+      </a>
+      <span className="flex-1 flex justify-end mr-[30px]">© 2025 Anurak</span>
+    </div>
+  );
+}
+export default Footer;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,25 @@
-function Header(){
-    return(
-        <div>
-            Header
-        </div>
-    )
-}
+import { Link } from "react-router";
 
-export default Header
+function Header() {
+  return (
+    <div className="p-0 h-[100px] w-100% grid grid-cols-2 items-center  bg-gradient-to-b from-[#4D0000] via-73%-[#9C0000] to-[#9C0000] z-index-2">
+      <div className="flex ml-[100px]">
+        <Link to={"/"}>
+          <span className="header">Cocktail Crafted</span>
+        </Link>
+      </div>
+      <div className="header-nav flex justify-end mr-[10px]">
+        <Link to={"/"}>
+          <span>HOME</span>
+        </Link>
+        <Link to={"/ingredient/"}>
+          <span>INGREDIENTS</span>
+        </Link>
+        <Link to={"/favorite/"}>
+          <span>FAVORITE</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
+export default Header;
